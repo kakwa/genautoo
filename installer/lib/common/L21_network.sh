20a07552b0e548e70a9bbe9224c7fd19a3787bc7
kakwa <kakwa@talisker.(none)>
2012-12-18 07:07:53 +0100

    adding the handling of the network config for the installed system

--- installer/lib/common/L21_network.sh
+++ installer/lib/common/L21_network.sh
@@ -1,11 +1,58 @@
-network_set_hostname(){
-    if ! [ -z "$hostname" ]
-    then
+network_handler(){
+    local line="$1"
+
+    local arg_1=`common_get_arg_n 1 "$line"`  
+    local arg_2=`common_get_arg_n 2 "$line"`  
+    local arg_3=`common_get_arg_n 3 "$line"`  
+    local arg_4=`common_get_arg_n 4 "$line"`  
+
+    if [ "$arg_1" = "route" ]
+    then  
+        echo "route=\"$arg_2 via $arg_4\"" >>/etc/conf.d/net
+
+    elif [ "$arg_1" = "hostname" ]
+    then  
 	    sed -i "s/hostname.*=.*/hostname="$hostname"/" /etc/conf.d/hostname
         hostname $hostname
+
+    elif [ "$arg_1" = "dns" ]
+    then 
+        echo "nameserver $arg_2" >>/tmp/resolv.conf
+    else
+        if [ "$arg_2" = "dhcp" ]
+        then 
+             echo "config_$arg_1=\"dhcp\"" >>/etc/conf.d/net
+             ln -s /etc/init.d/net.lo /etc/init.d/net.$arg_1
+             rc-update add net.$arg_1 default
+        elif [ "$arg_2" = "static" ]
+        then
+             echo "modules_$arg_1=\"ifconfig\"" >>/etc/conf.d/net
+             echo "config_$arg_1=\"$arg_3\"" >>/etc/conf.d/net
+             ln -s /etc/init.d/net.lo /etc/init.d/net.$arg_1
+             rc-update add net.$arg_1 default
+        fi
     fi
+
 }
 
-#network_configure_interface(){
-#local input=$1
-#}
+network_set_hostname_install(){
+    local input_file=$1
+
+    local cleaned_network_file=`mktemp`
+    common_cleanup_file $input_file $cleaned_network_file
+    local line_hostname=`grep "^hostname" $cleaned_network_file`
+    local hostname=`common_get_arg_n 2 "$line_hostname"`
+    hostname $hostname
+}
+
+network_configure(){
+    local input_file=$1
+
+    local cleaned_network_file=`mktemp`
+    common_cleanup_file $input_file $cleaned_network_file
+    echo 
+    while read line
+    do
+        network_handler "$line"
+    done <$cleaned_network_file
+}
