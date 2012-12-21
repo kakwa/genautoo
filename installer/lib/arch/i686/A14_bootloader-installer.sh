56fe0737bf7f138c73372e1a80566bcd2baef482
kakwa <kakwa@talisker.(none)>
2012-12-21 19:27:42 +0100

    adding the same comments inside i686 arch dir (same as amd64)

--- installer/lib/arch/i686/A14_bootloader-installer.sh
+++ installer/lib/arch/i686/A14_bootloader-installer.sh
@@ -1,18 +1,28 @@
-
 arch_bootloader_get_disks_list(){
+    #a small function listing the disks
     fdisk -l |grep "^Disk /dev/"| cut -d ' ' -f 2 | cut -b 1-8
 }
 
 arch_bootloader_get_grub_boot_disk(){
+    #function making the link between /dev/sd* and hd* (the "grub naming")
+    #$1 -> boot partition's name (/dev/sd* format)
     local boot_partition=$1
+
     local disk=`arch_bootloader_get_disk $boot_partition`
     grep "$disk$"  /boot/grub/device.map |sed "s/(//"|sed "s/).*//"
 }
 
 arch_bootloader_get_disk(){
+    #this function get the disk of a given partition 
+    #(arch_bootloader_get_disk sda1 -> sda)
+    #$1 -> the partition
+
     local partition=$1
+
     for d in `arch_bootloader_get_disks_list`
     do
+        #no, sed "s//[0-9]$/" doesn't work 
+        #(ex, compaq raid card: /dev/ida/c0d0, partition is /dev/ida/c0d0p1)
         echo $partition | grep -q $d
         ret=$?
         if [ $ret -eq 0 ]
@@ -23,6 +33,12 @@ arch_bootloader_get_disk(){
 }
 
 arch_get_grub_number(){
+    #function getting the number of a device (1 in /dev/sda1) and return 
+    #this number - 1
+    #it's used for sda1 -> (hd0,0) conversion for grub
+    #I am counting on the fact that linux starts at "1" the numbering of its devices
+    #don't know if it's always true...
+
     local boot_partition=$1
     local number=$((`echo $boot_partition |sed "s/.*\([0-9][0-9]*\)/\1/"` - 1  ))
 
@@ -30,6 +46,7 @@ arch_get_grub_number(){
 }
 
 arch_bootloader_install(){
+    #function configuring the bootloader
     
     grep -v rootfs /proc/mounts >/etc/mtab
 
@@ -43,6 +60,7 @@ arch_bootloader_install(){
 
         
     local grub_disk=`arch_bootloader_get_grub_boot_disk $boot_partition`
+    local boot_disk=`arch_bootloader_get_disk `
     local grub_number=`arch_get_grub_number $boot_partition`
 
 
