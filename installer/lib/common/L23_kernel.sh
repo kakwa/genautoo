d624fa9f97184088326c63b8427852e10ad69824
kakwa <kakwa@talisker.(none)>
2012-12-23 09:37:24 +0100

    comment inside L23_kernel.sh

--- installer/lib/common/L23_kernel.sh
+++ installer/lib/common/L23_kernel.sh
@@ -1,11 +1,14 @@
 kernel_install_kernel(){
-    #if we have put a .config
+    #this function compile the kernel 
+    #it uses genkernel (need to find a better way)
+
+    #if we have put a .config (not working know)
     if [ -f /usr/src/linux/.config ]
     then 
         cd /usr/src/linux/
         make $parallele_emerge && make modules_install $parallele_emerge && make install
     else
-        emerge $GLOBAL_PARALLELE_EMERGE genkernel
+        emerge $parallele_emerge genkernel
         cd /usr/src/linux/
         genkernel --makeopts="$parallele_emerge" --no-menuconfig all
     fi
