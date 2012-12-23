43cd1b90a5b40d38293d4f8adc9fe276136701b4
kakwa <kakwa@talisker.(none)>
2012-12-23 09:39:44 +0100

    commenting L24_bootloader.sh

--- installer/lib/common/L24_bootloader.sh
+++ installer/lib/common/L24_bootloader.sh
@@ -1,17 +1,24 @@
 bootloader_get_partition(){
+    #this function gets the partition of a given file
+    #or a given dir
+    #$1 the given file or dir
+
     local argument=$1
     grep -v rootfs /proc/mounts >/etc/mtab
     df -P $argument | tail -1 | cut -d' ' -f 1
 }
 
 bootloader_get_kernel(){
+    #find a kernel
     basename `ls /boot/kernel*|head -n 1`
 }
 
 bootloader_get_initrd(){
+    #find an initrd
     basename `ls /boot/initramfs*|head -n 1`
 }
 
 bootloader_install_bootloader(){
+    #install the bootloader
     arch_bootloader_install
 }
