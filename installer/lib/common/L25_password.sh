e8193891a9bb95654df6083e1c9ac4062bad2e65
kakwa <kakwa@talisker.(none)>
2012-12-23 09:46:23 +0100

    commenting L25_password.sh

--- installer/lib/common/L25_password.sh
+++ installer/lib/common/L25_password.sh
@@ -1,4 +1,8 @@
 password_set_password(){
+    #this function sets the root password
+    #if no root password specified, the default
+    #is "password"
+
     if [ -z $root_password ]
     then
         root_password="password"
