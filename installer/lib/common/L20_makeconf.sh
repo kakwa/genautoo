9c0637c9dd9a4e328f99f8b316005dacc1096806
kakwa <kakwa@talisker.(none)>
2012-12-23 09:20:16 +0100

    adding comments inside L20_makeconf.sh

--- installer/lib/common/L20_makeconf.sh
+++ installer/lib/common/L20_makeconf.sh
@@ -1,4 +1,6 @@
 makeconf_add_makeconf_section(){
+    #this function updates the make.conf (it adds the line 
+    #of the section make.conf to the existing make.conf)
     tmp=`mktemp`
     cat $GLOBAL_MAKECONF_PATH $GLOBAL_CHROOT_DIR_INSTALLER/$GLOBAL_SPLITTED_DIR/make.conf >$tmp
     mv $tmp $GLOBAL_MAKECONF_PATH
